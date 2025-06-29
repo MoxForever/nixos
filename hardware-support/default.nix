eb12937dfc4dfa94169030b4f9dac0b0d920c520
= <=>
2025-06-29 16:38:10 +0600

    huita ebanaya

--- hardware-support/default.nix
+++ hardware-support/default.nix
@@ -1,11 +1,7 @@
 { config, lib, pkgs, ... }:
 
 {
-  options.hardware.hasNvidiaCard = builtins.any 
-    (gpu: builtins.match ".*NVIDIA.*" gpu != null) 
-    (lib.splitString "\n" (builtins.readFile "${pkgs.pciutils}/bin/lspci"));
-
-  config = {
-    imports = lib.optional config.hardware.hasNvidiaCard ./nvidia.nix;
-  };
+  imports = [
+    ./nvidia.nix
+  ]
 }
